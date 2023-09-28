5ef1218211cea707ada1711e8f99ca80dbced368
squaredumplings <mihnea.buzoiu@gmail.com>
2023-09-28 12:46:36 +0200

    Take

---
+++ Assignment4/SudokuSolver.java
@@ -0,0 +1,16 @@
+public class SudokuSolver {
+
+    public SudokuSolver(SudokuGrid grid) {
+        // Initialize the SudokuSolver with the provided SudokuGrid
+    }
+
+    public boolean solve() {
+        // Use a recursive strategy to solve the Sudoku puzzle
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // Create a SudokuGrid and a SudokuSolver
+        // Call solve() to solve the puzzle and print the solution or a message if no solution is found
+    }
+}
